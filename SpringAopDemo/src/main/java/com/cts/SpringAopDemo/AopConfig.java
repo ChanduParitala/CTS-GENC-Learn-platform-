5845b0bf102e49745389e547b1f37d779d73ed81
Chandu Paritala <60039091+ChanduParitala@users.noreply.github.com>
2021-05-30 12:52:00 +0530

    Add files via upload

---
+++ SpringAopDemo/src/main/java/com/cts/SpringAopDemo/AopConfig.java
@@ -0,0 +1,17 @@
+package com.cts.SpringAopDemo;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+//Type your code here
+//Type your code here
+//Type your code here
+@ComponentScan
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+	
+	
+}
